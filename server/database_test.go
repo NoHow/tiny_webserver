57f2114e2e737f7f713ae8273eb7273ba672d6c2
Nazar Kushchenko <nkushenko@gmail.com>
2021-11-24 19:08:46 +0200

    Fixed tests and added possibiltiy to run in parallel

--- server/database_test.go
+++ server/database_test.go
@@ -3,7 +3,10 @@ package server
 import (
 	"github.com/boltdb/bolt"
 	"github.com/matryer/is"
+	"log"
 	"os"
+	"strconv"
+	"sync"
 	"testing"
 	"tinywebserver/utils"
 )
@@ -13,19 +16,56 @@ var defaultTestUserData = TwsUserData{
 		AvatarUrl:  "testAvatarUrl.com",
 		AdminRight: 0,
 		IsLogged:   false,
+}
+
+var testDBFolder = "test_databases"
+var dbGeneratorLock = sync.Mutex{}
+var dbFilesCounter = 0
+
+func generateTestDB(is *is.I) (*bolt.DB, func()) {
+	dbGeneratorLock.Lock()
+	err := os.Mkdir(testDBFolder, 0700)
+	if err != nil && !os.IsExist(err){
+		log.Fatalln(err)
 	}
+	testDBName := testDBFolder + "/test_tws" +  strconv.Itoa(dbFilesCounter) + ".db"
+	dbFilesCounter++
+	defer dbGeneratorLock.Unlock()
+
+	db, err := bolt.Open(testDBName, 0600, nil)
+	is.NoErr(err)
+	
+	return db, func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = os.Remove(testDBName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		dbGeneratorLock.Lock()
+		dbFilesCounter--
+		if dbFilesCounter == 0 {
+			err := os.Remove(testDBFolder)
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		dbGeneratorLock.Unlock()
+	}
+}
 
 func TestSavePost(t *testing.T) {
+	t.Parallel()
 	is := is.New(t)
-
-	db, err := bolt.Open("test_tws.db", 0600, nil)
-	is.NoErr(err)
-	defer db.Close()
-	defer os.Remove("test_tws.db")
+	db, cleanUp := generateTestDB(is)
+	defer cleanUp()
 	testDB := twsDB{ db: db }
-	
+
 	//Test users bucket doesn't exist scenario
-	_, err = testDB.savePost([]byte(defaultTestUserData.UserID), dbPost{})
+	_, err := testDB.savePost([]byte(defaultTestUserData.UserID), dbPost{})
 	is.True(err != nil)
 
 	//Test successful save scenario
@@ -45,17 +85,16 @@ func TestSavePost(t *testing.T) {
 }
 
 func TestDeleteUserPost(t *testing.T) {
+	t.Parallel()
 	is := is.New(t)
-	db, err := bolt.Open("test_tws.db", 0600, nil)
-	is.NoErr(err)
-	defer db.Close()
-	defer os.Remove("test_tws.db")
+	db, cleanUp := generateTestDB(is)
+	defer cleanUp()
 	testDB := twsDB{ db: db }
 
 	//Test successful delete scenario
 	createBucketIfNotExistsOrDie([]byte("Users"), testDB.db)
 	createBucketIfNotExistsOrDie([]byte("Posts"), testDB.db)
-	_, err = testDB.SyncUser(defaultTestUserData)
+	_, err := testDB.SyncUser(defaultTestUserData)
 	testPost := dbPost{
 		Text:	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim ID est laborum.",
 	}
@@ -69,17 +108,16 @@ func TestDeleteUserPost(t *testing.T) {
 }
 
 func TestLikeUserPost(t *testing.T) {
+	t.Parallel()
 	is := is.New(t)
-	db, err := bolt.Open("test_tws.db", 0600, nil)
-	is.NoErr(err)
-	defer db.Close()
-	defer os.Remove("test_tws.db")
+	db, cleanUp := generateTestDB(is)
+	defer cleanUp()
 	testDB := twsDB{ db: db }
 
 	//Test successful like and unlike scenario
 	createBucketIfNotExistsOrDie([]byte("Users"), testDB.db)
 	createBucketIfNotExistsOrDie([]byte("Posts"), testDB.db)
-	_, err = testDB.SyncUser(defaultTestUserData)
+	_, err := testDB.SyncUser(defaultTestUserData)
 	testPost := dbPost{
 		Text:	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim ID est laborum.",
 	}
